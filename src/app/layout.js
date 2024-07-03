b2b85f60c86526bbe9ec18b452dd44f9e3e6fc1a
Akki-net <akashsharma.zak@gmail.com>
2024-07-03 02:46:23 -0700

    login Form

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import AuthProvider from "@/components/AuthProvider";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function RootLayout({ children }) {
   return (
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,24 +1,45 @@
 "use client";
 import React from 'react'
 import { signIn, useSession } from "next-auth/react"
+import { Form, Row, Col, Button, Container } from 'react-bootstrap';
 
 function Login() {
     const session = useSession();
     console.log(session);
 
-    if(session.status == "loading")
+    if (session.status == "loading")
         return <p>Loading...</p>
 
-    if(session.status == "authenticated")
+    if (session.status == "authenticated")
         return <p>user authenticated</p>
 
-    if(session.status == "unauthenticated")
-        return <p>user unauthenticated</p>
-
     return (
-        <div>
-            <button onClick={() => signIn("google")}>Login with google</button>
-        </div>
+        <Container className='p-5'>
+            <Form>
+                <Form.Group as={Row} className='mb-3'>
+                    <Form.Label column sm="2">
+                        Email
+                    </Form.Label>
+                    <Col sm={{ offset: 1, span: 9 }}>
+                        <Form.Control />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className='mb-3'>
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm={{ offset: 1, span: 9 }}>
+                        <Form.Control type="password" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} sm="4" className='mb-3 justify-content-center'>
+                    <Button variant='success'>Log In</Button>
+                </Form.Group>
+                <Form.Group as={Row} sm="3" className='mb-3 justify-content-center'>
+                    <Button variant='danger' onClick={() => signIn("google")}>Login with google</Button>
+                </Form.Group>
+            </Form>
+        </Container>
     )
 }
 
